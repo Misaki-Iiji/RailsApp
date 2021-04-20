aa90b5a2a8f6118b76866f885713901346cc3847
Misaki-Iiji <misahiro33@gmail.com>
2021-04-20 19:41:17 +0900

    コントローラーに用意した値

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,18 +1,6 @@
 class HelloController < ApplicationController
   def index
-    if params['msg'] != nil then
-      msg = 'Hello, ' + params['msg'] + '!'
-    else
-      msg = 'This is sample page'
-    end
-    html = '
-    <html>
-    <body>
-    <h1>Sample Page</h1>
-    <p>' + msg + '</p>
-    </body>
-    </html>
-    '
-    render html: html.html_safe
+    @title = "View Sample"
+    @msg = "コントローラーに用意した値です"
   end
 end
