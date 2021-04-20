96f1f827b76aa1ad729a41711b95036e99eb8be5
Misaki-Iiji <misahiro33@gmail.com>
2021-04-20 18:39:30 +0900

    msg

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,18 @@
 class HelloController < ApplicationController
   def index
-    render plain: "Hello,This is rails sample page!"
+    if params['msg'] != nil then
+      msg = 'Hello, ' + params['msg'] + '!'
+    else
+      msg = 'This is sample page'
+    end
+    html = '
+    <html>
+    <body>
+    <h1>Sample Page</h1>
+    <p>' + msg + '</p>
+    </body>
+    </html>
+    '
+    render html: html.html_safe
   end
 end
