25075a0a7ab60ae38d66aa2ae453ada9a3a1b864
Misaki-Iiji <misahiro33@gmail.com>
2021-04-24 18:25:32 +0900

    people add create

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
+
   get 'msgboard', to:'msgboard#index'
   post 'msgboard', to:'msgboard#index'
   get 'msgboard/index'
   post 'msgboard/index'
+
   get 'hello/index'
   get 'hello', to:'hello#index'
   get 'hello/other'
