73593f1385f6c8efbc6b123fd04cc740e81d8073
Misaki-Iiji <misahiro33@gmail.com>
2021-04-28 18:54:43 +0900

    person_params

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -11,16 +11,18 @@ class PeopleController < ApplicationController
 
   def add
     @msg = "add new data."
+    @person = Person.new
   end
 
   def create
     if request.post? then
-      obj = Person.create(
-        name: params['name'],
-        age: params['age'],
-        mail: params['mail']
-      )
+      Peason.create(person_params)
     end
     redirect_to '/people'
+  end
+
+  private
+  def person_params
+    params.require(:peason).permit(:name, :age, :mail)
   end
 end
