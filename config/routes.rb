b0a3d3c92f47c4928086967626432d58e7c2290a
Misaki-Iiji <misahiro33@gmail.com>
2021-04-29 14:09:50 +0900

    読書カードfinish

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   get 'cards/index'
-  get 'cards/show'
+  get 'cards', to: 'cards#index'
   get 'cards/add'
-  get 'cards/edit'
-  
+  post 'cards/add'
+  get 'cards/:id', to: 'cards#show'
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+  get 'cards/delete/:id', to: 'cards#delete'
+
   get 'people/index'
   get 'people', to: 'people#index'
   get 'people/add'
   post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
   get 'people/edit/:id', to: 'people#edit'
-  # post 'people/edit/:id', to: 'people#update'
   patch 'people/edit/:id', to: 'people#update'
   get 'people/delete/:id', to: 'people#delete'
 
